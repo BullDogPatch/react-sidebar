c2f178f5361f85f2a3a38c5f558d5561cf0bb863
Craig Clayton <55770352+BullDogPatch@users.noreply.github.com>
2020-08-30 22:26:46 +0100

    ... I need to make it so when you click outside the sidebar then the sidebar disappears

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Reports from "./components/pages/Reports";
 
 function App() {
   return (
-    <>
+    <div>
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" />
+          <Route exact path="/" component={Home} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/products" component={Products} />
         </Switch>
       </Router>
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { SidebarData } from "./SidebarData";
+import { IconContext } from "react-icons";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -15,28 +16,30 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="navbar">
-        <Link to="#" className="menu-bars">
-          <FaIcons.FaBars onClick={showSidebar} />
-        </Link>
-      </div>
-      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-        <ul className="nav-menu-items">
-          <li className="navbar-toggle">
-            <Link to="#" className="menu-bars">
-              <AiIcons.AiOutlineClose />
-            </Link>
-          </li>
-          {SidebarData.map((data, index) => (
-            <li key={index} className={data.c_name}>
-              <Link to={data.path}>
-                {data.icon}
-                <span>{data.title}</span>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
               </Link>
             </li>
-          ))}
-        </ul>
-      </nav>
+            {SidebarData.map((data, index) => (
+              <li key={index} className={data.c_name}>
+                <Link to={data.path}>
+                  {data.icon}
+                  <span>{data.title}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </IconContext.Provider>
     </>
   );
 };
